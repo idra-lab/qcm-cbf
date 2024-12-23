751c0896a9211d8ae130efbde433b9d570bfcb08
Matteo Saveriano <matteosaveriano86@gmail.com>
2024-12-23 09:15:18 +0100

    Removed video making stuff

--- example/demo_office_navigation.m
+++ example/demo_office_navigation.m
@@ -173,11 +173,6 @@ for i = 1 : Nobst
     vertices_y{end+1} = realWorld.obstacles{i}.contour(2,end:-1:1);
 end
 
-%% init
-% create the video writer with 1 fps
-writerObj = VideoWriter('myVideo.avi');
-writerObj.FrameRate = 30;
-
 % real world
 % x = [-2;2.25]; col_ = 1;
 % x = [-2;2.25]; col_ = 2;
@@ -350,14 +345,3 @@ for t = 2 : T_MAX
     F(t) = getframe(gcf);
     drawnow
 end
-
-% open the video writer
-open(writerObj);
-% write the frames to the video
-for i=1:length(F)
-    % convert the image to a frame
-    frame = F(i) ;    
-    writeVideo(writerObj, frame);
-end
-% close the writer object
-close(writerObj);
